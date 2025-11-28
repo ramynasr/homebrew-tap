45e8a69df4533331fedbd98ad8a39dade7b6c148
Ramy Nasr <ramy.nasr@automattic.com>
2025-11-28 16:17:43 -0500

    Update linear-for-ai to v0.1.0

--- Formula/linear-for-ai.rb
+++ Formula/linear-for-ai.rb
@@ -7,20 +7,20 @@ class LinearForAi < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ramynasr/linear-for-ai/releases/download/v0.1.0/linear-for-ai-macos-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/ramynasr/linear-for-ai/releases/download/v0.1.0/linear-for-ai-macos-x64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ramynasr/linear-for-ai/releases/download/v0.1.0/linear-for-ai-linux-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/ramynasr/linear-for-ai/releases/download/v0.1.0/linear-for-ai-linux-x64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
